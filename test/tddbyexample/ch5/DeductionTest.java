e237839d7662923cb7bccc7cc4b743e2c1c5d84e
Ivan Lipovic <ivan.lipovic@gmail.com>
2012-04-03 01:43:26 -0700

    resolving conflicting circumstances

--- test/tddbyexample/ch5/DeductionTest.java
+++ test/tddbyexample/ch5/DeductionTest.java
@@ -14,11 +14,31 @@ public class DeductionTest {
 		assertTrue(ivan.valid());
 	}
 	
+	
+	
 	private Set<Circumstance> observation(){
 		Set<Circumstance> circumstances = new LinkedHashSet<Circumstance>();
 		Circumstance nickStatement = new Circumstance("Earlier in the night, Nick said he enjoys comfortable footwear");
 		circumstances.add(nickStatement);
 		return circumstances;
 	}
+	@Test
+	public void testDeductionFromMultipleConflictingCircumstances() {
+		Set<Circumstance> circumstances = sexStory();
+		Deduction ivan = new Deduction(circumstances);
+		assertFalse(ivan.valid());
+	}
 
+
+
+	private Set<Circumstance> sexStory() {
+		Set<Circumstance> circumstances = new LinkedHashSet<Circumstance>();
+		Circumstance shaniquaStatement = new Circumstance("Shaniqua came into the bar proclaiming Ivan was her BABBY DADDY!");
+		Circumstance ivanStatement = new Circumstance("Ivan stated he never slept with this crazy bitch so he couldn't be her Babby daddy she just wants all his money he made from the byter IPO");
+		Circumstance dnaStatement = new Circumstance("DNA sequencing found that Ivan had indeed slept with Shaniqua but was too ashamed to admit it in public");
+		circumstances.add(shaniquaStatement);
+		circumstances.add(ivanStatement);
+		circumstances.add(dnaStatement);
+		return circumstances;
+	}
 }
